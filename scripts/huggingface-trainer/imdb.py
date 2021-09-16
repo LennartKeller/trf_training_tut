3e5a8d6af114b32dc05b5f72e3f6126a1ecad9ab
Lennart Keller <lennart.keller@uni-wuerzburg.de>
2021-09-16 13:10:53 +0200

    update hugginface trainer stuff

--- scripts/huggingface-trainer/imdb.py
+++ scripts/huggingface-trainer/imdb.py
@@ -1,20 +1,77 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer
-from datasets.load import load_dataset
-from transformers.training_args import TrainingArguments
+from transformers import Trainer, TrainingArguments, TrainerCallback
+from transformers import BertTokenizerFast
+from transformers import set_seed
+from model import BertForMultiHeadConfig, BertForMultiHeadModel
+from datasets import load_from_disk
 
 
-def init_model(*args)
+def make_tokenization_func(tokenizer, text_column, *args, **kwargs):
+    def tokenization(entry):
+        return tokenizer(entry[text_column], *args, **kwargs)
+    return tokenization
+
+
+def make_rename_func(mapping, remove_src=False):
+    def rename(entry):
+        for src, dst in mapping.items():
+            if remove_src:
+                data = entry.pop(src)
+            else:
+                data = entry[src]
+            entry[dst] = data
+        return entry
+    return rename
 
 if __name__ == '__main__':
-    model = AutoModelForSequenceClassification.from_pretrained('distilbert-base-cased')
-    tokenizer = AutoTokenizer.from_pretrained('distilbert-base-cased')
+    set_seed(42)
 
-    dataset = load_dataset('imdb')
+    dataset = load_from_disk('../data/imdb_huggingface')
+    
+    num_classes = dataset['train'].features['titleType'].num_classes
+    num_labels = dataset['train'].features['genre'].feature.num_classes
 
-    dataset = dataset.map(lambda entry: tokenizer(entry['text'], padding=True, truncation=True))
-    dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
-    dataset = dataset.rename_column('label', 'labels')
+    tokenizer = BertTokenizerFast.from_pretrained('bert-base-german-cased')
+    model_config = BertForMultiHeadConfig.from_pretrained('bert-base-german-cased', num_labels=num_classes, num_multi_labels=num_labels)
+    model = BertForMultiHeadModel.from_pretrained('bert-base-german-cased', config=model_config)
 
-    trainer = Trainer(mode,)
+    tokenization = make_tokenization_func(tokenizer=tokenizer, text_column='text', padding=True, truncation=True)
+    dataset = dataset.map(tokenization, batched=True)
+    
+    rename_target_columns = make_rename_func({'titleType': 'labels', 'genre': 'multi_labels', 'averageRating': 'regression_targets'})
+    dataset = dataset.map(rename_target_columns, batched=True)
+
+    dataset.set_format('torch')
+
+    training_args = TrainingArguments(
+        output_dir='test',
+        overwrite_output_dir=True,
+        learning_rate=10e-5,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=8,
+        evaluation_strategy='steps',
+        eval_steps=200,
+        num_train_epochs=5,
+        logging_dir='test/logs',
+        logging_steps=200,
+        save_strategy='steps',
+        save_steps=2000,
+        remove_unused_columns=True
+        )
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['val']
+    )
+
+    trainer.train()
+    
+    trainer.save_model('test/model')
+
+
+
+
+
 
 
--- scripts/huggingface-trainer/model.py
+++ scripts/huggingface-trainer/model.py
@@ -47,16 +47,38 @@ class BertForMultiHeadModel(PreTrainedModel):
         self.regression_head = nn.Linear(in_features=config.hidden_size, out_features=1)
         self.init_weights()
     
-    def forward(self, *args, **kwargs):
+    def forward(self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                labels=None,
+                multi_labels=None,
+                regression_targets=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                return_dict=None
+               ):
         
-        classification_labels = kwargs.pop('labels', None)
-        multilabel_classification_labels = kwargs.pop('multi_labels', None)
-        regression_targets = kwargs.pop('regression_targets', None)
+        classification_labels = labels
+        multilabel_classification_labels = multi_labels
         
-        kwargs['return_dict'] = True
-        base_outputs = self.bert(*args, **kwargs)
+        return_dict = True
+        base_outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict
+            )
         
-        pooled_representation = base_outputs.last_hidden_state[:, 0, :] # cls
+        pooled_representation = base_outputs.last_hidden_state.mean(dim=1) # cls
         
         classification_logits = self.classification_head(pooled_representation)
         multilabels_logits = self.multilabel_classification_head(pooled_representation)
@@ -68,16 +90,24 @@ class BertForMultiHeadModel(PreTrainedModel):
         
         losses = []
         if classification_labels is not None:
+            #print(classification_logits.size())
             predictions = classification_logits.view(-1, self.config.num_labels)
+            #print(predictions.size())
             targets = classification_labels.view(-1)
+            #print(predictions.argmax(dim=1))
             loss_func = nn.CrossEntropyLoss()
             classification_loss = loss_func(predictions, targets)
             base_outputs['classification_loss'] = classification_loss
             losses.append(classification_loss)
         
         if multilabel_classification_labels is not None:
-            predictions = multilabels_logits.view(-1, self.config.num_labels)
-            targets = multilabel_classification_labels.view(-1, self.config.num_labels)
+            #print(multilabels_logits.size())
+            predictions = multilabels_logits.view(-1, self.config.num_multi_labels)
+            #print(predictions)
+            #print(predictions.size())
+            targets = multilabel_classification_labels.view(-1, self.config.num_multi_labels).float()
+            #print(predictions.topk(k=3).indices)
+            #print(targets)
             loss_func = nn.BCEWithLogitsLoss()
             multilabel_classification_loss = loss_func(predictions, targets)
             base_outputs['multilabel_classification_loss'] = classification_loss
@@ -85,14 +115,17 @@ class BertForMultiHeadModel(PreTrainedModel):
         
         if regression_targets is not None:
             predictions = regression_logits.view(-1)
+            #print(predictions)
             targets = regression_targets.view(-1)
+            #print(predictions.size())
             loss_func = nn.MSELoss()
             regression_loss = loss_func(predictions, targets)
             base_outputs['regression_loss'] = regression_loss
             losses.append(regression_loss)
         
         if losses:
-            loss = torch.mean(torch.stack(losses), dim=-1)
+            print(losses)
+            loss = torch.stack(losses).sum(dim=-1)
             base_outputs['loss'] = loss
             
         return base_outputs
