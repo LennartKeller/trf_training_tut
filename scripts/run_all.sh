258f6d03866e449c28df25811d486e8d382036b4
Lennart Keller <lennart.keller@uni-wuerzburg.de>
2021-11-14 00:26:10 +0100

    fix bash script

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -3,16 +3,16 @@ export CUDA_VISIBLE_DEVICES="0"
 
 printf "\n\nRunning Huggingface Trainer\n\n"
 printf "\nnBert\nn"
-(cd huggingface-trainer && b bash run_rocstories_bert.sh)
+(cd huggingface-trainer && bash run_rocstories_bert.sh)
 printf "\Roberta\nn"
-(cd huggingface-trainer && b bash run_rocstories_roberta.sh)
+(cd huggingface-trainer && bash run_rocstories_roberta.sh)
 printf "\Distilbert\nn"
-(cd huggingface-trainer && b bash run_rocstories_distilbert.sh)
+(cd huggingface-trainer && bash run_rocstories_distilbert.sh)
 
 printf "\n\nRunning Pytorch Lightning\n\n"
 printf "\nnBert\nn"
-(cd pytorch-lightning && b bash run_rocstories_bert.sh)
+(cd pytorch-lightning && bash run_rocstories_bert.sh)
 printf "\Roberta\nn"
-(cd pytorch-lightning && b bash run_rocstories_roberta.sh)
+(cd pytorch-lightning && bash run_rocstories_roberta.sh)
 printf "\Distilbert\nn"
-(cd pytorch-lightning && b bash run_rocstories_distilbert.sh)
+(cd pytorch-lightning && bash run_rocstories_distilbert.sh)
