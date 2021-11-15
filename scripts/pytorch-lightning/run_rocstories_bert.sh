5149d86bb7282b7444416f2cf6cbd6c617e5576f
Lennart Keller <lennart.keller@googlemail.com>
2021-11-15 14:48:49 +0100

    update train params

--- scripts/pytorch-lightning/run_rocstories_bert.sh
+++ scripts/pytorch-lightning/run_rocstories_bert.sh
@@ -5,12 +5,13 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 python rocstories.py \
     --model.lr 3e-5 \
     --model.model_name_or_path bert-base-cased \
-    --run.train_batch_size 8 \
-    --run.val_batch_size 16 \
+    --run.train_batch_size 2 \
+    --run.val_batch_size 4 \
     --run.seed 42 \
+    --checkpoint.save_last true \
+    --checkpoint.every_n_train_steps 1000 \
     --tensorboard.name "rocstories_bert" \
     --tensorboard.save_dir "lightning_runs/logs" \
-    --trainer.gradient_clip_val 1.0 \
     --trainer.num_processes 10 \
     --trainer.val_check_interval 200 \
     --trainer.log_every_n_steps 50 \
--- scripts/pytorch-lightning/run_rocstories_roberta.sh
+++ scripts/pytorch-lightning/run_rocstories_roberta.sh
@@ -4,13 +4,14 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 python rocstories.py \
     --model.lr 3e-5 \
-    --model.model_name_or_path bert-base-cased \
-    --run.train_batch_size 4 \
-    --run.val_batch_size 8 \
+    --model.model_name_or_path roberta-base \
+    --run.train_batch_size 2 \
+    --run.val_batch_size 4 \
     --run.seed 42 \
-    --tensorboard.name "rocstories_roberta" \
-    --tensorboard.save_dir "lightning_runs/logs" \
-    --trainer.gradient_clip_val 1.0 \
+    --checkpoint.save_last true \
+    --checkpoint.every_n_train_steps 1000 \
+    --tensorboard.name rocstories_roberta \
+    --tensorboard.save_dir lightning_runs/logs \
     --trainer.num_processes 10 \
     --trainer.val_check_interval 200 \
     --trainer.log_every_n_steps 50 \
